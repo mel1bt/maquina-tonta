a62d5b9438acbae4214f26e7880b77cc62b74082
mel1bt <mberrocal.tipte@gmail.com>
2016-09-16 11:10:37 -0500

    version final

--- js/main.js
+++ js/main.js
@@ -1,64 +1,41 @@
 window.addEventListener("load", function() {
-   var supercri = document.getElementById("supercri");
-   supercri.addEventListener("click", function() { 
-    var color = document.getElementById("color").value;
-
-    if(color == "blue"){
-      document.getElementById("blue").classList.toggle("border");
-      document.getElementById("red").classList.remove("border");
-      document.getElementById("yellow").classList.remove("border");
-    } 
-      else if(color == "red"){
-      document.getElementById("red").classList.toggle("border");
-      document.getElementById("blue").classList.remove("border");
-      document.getElementById("yellow").classList.remove("border");
+	var boton = document.getElementById("cri");
+	var cont = 1;
+	boton.addEventListener("click", function(){
+		if (cont == 1){
+			document.getElementById("entrada").value= "rojo";
+            document.getElementById("one").classList.add("borde-rojo");
+            document.getElementById("three").classList.remove("borde-amarillo");
+		}
+		if (cont == 2){
+			document.getElementById("entrada").value = "azul";
+			document.getElementById("two").classList.add("borde-azul");
+            document.getElementById("one").classList.remove("borde-rojo");
+		}
+		if (cont == 3){
+			document.getElementById("entrada").value = "amarillo";
+			document.getElementById("three").classList.add("borde-amarillo");
+            document.getElementById("two").classList.remove("borde-azul");
+			cont = 0;
+		}
+		cont ++;
+	});
+	var boton = document.getElementById("super-cri");
+	boton.addEventListener("click", function(){
+    var entrada = document.getElementById("entrada").value;
+    if(entrada == "rojo"){
+        document.getElementById("one").classList.add("borde-rojo");
+        document.getElementById("two").classList.remove("borde-azul");
+        document.getElementById("three").classList.remove("borde-amarillo"); 
+    } else if(entrada == "azul"){
+        document.getElementById("one").classList.remove("borde-rojo");
+        document.getElementById("two").classList.add("borde-azul");
+        document.getElementById("three").classList.remove("borde-amarillo");        
+    }else if(entrada == "amarillo"){
+        document.getElementById("one").classList.remove("borde-rojo");
+        document.getElementById("two").classList.remove("borde-azul");
+        document.getElementById("three").classList.add("borde-amarillo");
     }
-      else if(color == "yellow"){
-      document.getElementById("yellow").classList.toggle("border");
-      document.getElementById("red").classList.remove("border");
-      document.getElementById("blue").classList.remove("border");
-    }
-
   });
+});
 
- });
-
-/*
-window.addEventListener("load", function() {
-	var boton = document.getElementById("cry");
-	boton.addEventListener("click", function() {
-		normalcry();
-	});
-
-	function normalcry(){
-        
-    var x = document.getElementsByClassName("circle");
-    var texto = document.getElementById("color");
-    var i=0;
-        if (i<3){
-            i=0;
-        }
-           
-        
-
-     x[i].style.border = "8px solid yellow";
-     i++;
-	 texto.innerHTML = document.getElementsByClassName("circle")[0].getAttribute("name");
-        
-    }
-    
-    var boton = document.getElementById("s-cry");
-	boton.addEventListener("click", function() {	
-        supercry();
-	});
-    function supercry(){
-        
-    var x = document.getElementsByClassName("circle");
-    var texto = document.getElementById("color");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.border = "3px solid blue";
-		texto.innerHTML = document.getElementsByClassName("circle")[0].getAttribute("name");
-        } 
-    }
-});*/
